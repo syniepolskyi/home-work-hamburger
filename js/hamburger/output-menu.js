6aaeeba24ad30f6ec2649a3e5b316a1d0252787c
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-18 01:10:23 +0300

    Update output-menu.js

--- js/hamburger/output-menu.js
+++ js/hamburger/output-menu.js
@@ -1,1 +1,20 @@
-
+Hamburger.renderMenu = function(){
+  const tableToppingsBody = document.querySelector(".table-toppings tbody");
+  const htmlStuffing = Object.keys(this.stuffing).map((k) => {
+    return `<tr><td>${this.stuffing[k].type}</td>`
+      +`<td>${this.stuffing[k].price}</td>`
+      +`<td>${this.stuffing[k].ccal}</td></tr>`;
+  }).join('');
+  tableToppingsBody.innerHTML = '';
+  tableToppingsBody.insertAdjacentHTML("beforeend",htmlStuffing);
+  //
+  //
+  const tableSizesBody = document.querySelector(".table-sizes tbody");
+  const htmlSizes = Object.keys(this.sizes).map((k) => {
+    return `<tr><td>${k}</td>`
+      +`<td>${this.sizes[k].price}</td>`
+      +`<td>${this.sizes[k].ccal}</td></tr>`;
+  }).join('');
+  tableSizesBody.innerHTML = '';
+  tableSizesBody.insertAdjacentHTML("beforeend",htmlSizes);
+}
