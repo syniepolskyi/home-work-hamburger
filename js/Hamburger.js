6f46ea6269daa56bca5eebce59dae90708a29b71
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-18 01:00:43 +0300

    Create Hamburger.js

---
+++ js/Hamburger.js
@@ -0,0 +1,38 @@
+class Hamburger {
+  static sizes = {
+    S: {
+      price: 200,
+      ccal: 500
+    },
+    M: {
+      price: 300,
+      ccal: 700
+    },
+    L: {
+      price: 400,
+      ccal: 1000
+    }
+  }
+  static stuffing = {
+    STUFFING_SALAD: {
+      type: "STUFFING SALAD",
+      price: 300,
+      ccal: 200
+    },
+    STUFFING_POTATO: {
+      type: "STUFFING POTATO",
+      price: 400,
+      ccal: 100
+    },
+    TOPPING_MAYO: {
+      type: "TOPPING MAYO",
+      price: 300,
+      ccal: 400
+    },
+    TOPPING_SPICE: {
+      type: "TOPPING SPICE",
+      price: 500,
+      ccal: 700
+    },
+  }
+}
