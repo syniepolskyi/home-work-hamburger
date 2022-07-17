d2dee3437ec190071722522a7f4d45469dc18dff
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-17 14:17:44 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 class Hamburger {
-
+  static stuffing = {
+    STUFFING_SALAD: {
+      type: "STUFFING SALAD",
+      price: 300,
+      ccal: 200
+    },
+    STUFFING_POTATO: {
+      type: "STUFFING POTATO",
+      price: 400,
+      ccal: 100
+    },
+    TOPPING_MAYO: {
+      type: "TOPPING MAYO",
+      price: 300,
+      ccal: 400
+    },
+    TOPPING_SPICE: {
+      type: "TOPPING SPICE",
+      price: 500,
+      ccal: 700
+    },
+  }
+  
+  
 }
