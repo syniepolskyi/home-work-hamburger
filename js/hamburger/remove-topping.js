656f109142a62d66d65ef88d3790199aea50d2c3
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-23 01:24:58 +0300

    Update remove-topping.js

--- js/hamburger/remove-topping.js
+++ js/hamburger/remove-topping.js
@@ -1,4 +1,24 @@
 Hamburger.removeTopping = function(){
   //Your code here
+  const removeBtns = document.querySelectorAll(".order-table button.remove");
+  const remove = (btn) => {
+    const elTr = btn.parentNode.parentNode;
+    const amountTd = elTr.querySelector("td.data-amount");
+    let amount = parseInt(amountTd.innerText, 10);
+    if (amount > 1){
+      amount--;
+      amountTd.innerText = amount;
+      return ;
+    }
+    elTr.remove();
+  };
+  
+  Array.from(removeBtns)
+    .forEach(el => {
+      el.addEventListener("click", (ev) => 
+        remove(( ev.target.nodeName !== "BUTTON" ? 
+          ev.target.parentNode : ev.target )) 
+      )
+  });
   
 }
