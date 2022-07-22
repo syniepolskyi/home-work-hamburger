39064d0e19a2e7cd40bf6c562600e2ec1573baab
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-22 23:03:39 +0300

    Update choose-size.js

--- js/hamburger/choose-size.js
+++ js/hamburger/choose-size.js
@@ -1,4 +1,28 @@
 Hamburger.chooseSize = function(){
-  //Your code here
+  const inputSize = document.getElementById("size");
+  const sizeAcList = inputSize.nextElementSibling.firstElementChild;
+  const addToppingBtn = document.getElementById("addtopping");
+  const submitOrderBtn = document.getElementById("submitorder");
+  const orderTableBody = document.querySelector(".order-table tbody");
+  
+  const choose = (sizeKey) => {
+    const trTemplate = document.querySelector("tr.order-template");
+    inputSize.value = Hamburger.sizes[sizeKey].name;
+    submitOrderBtn.removeAttribute("disabled");
+    const newTr = document.createElement("TR");
+    newTr.classList.add("active-data");
+    newTr.innerHTML = trTemplate.innerHTML;
+    newTr.firstElementChild.innerHTML = '';
+    orderTableBody.appendChild(newTr);
+    Hamburger.calculateTotal();
+    sizeAcList.classList.toggle("hidden", true);
+  };
+  
+  Array.from(sizeAcList).forEach((el) => 
+    el.addEventListener("click", ev => {
+      ev.preventDefault();
+      choose( ev.target.getAttribute("data-size") );
+    })
+  );
   
 }
