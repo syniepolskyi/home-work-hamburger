57013bf3ec466e6a53a12553007fc06748dd3836
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-23 00:46:09 +0300

    Update choose-topping.js

--- js/hamburger/choose-topping.js
+++ js/hamburger/choose-topping.js
@@ -1,4 +1,22 @@
 Hamburger.chooseTopping = function(){
-  //Your code here
+  const inputTopp = document.getElementById("topping");
+  const topppAcList = inputTopp.nextElementSibling.nextElementSibling.firstElementChild;
+  const elemLinks = topppAcList.querySelectorAll("a");
+  const addToppingBtn = document.getElementById("addtopping");
+  
+  const choose = (stuffKey) => {
+    inputTopp.value = Hamburger.stuffing[stuffKey].type;
+    inputTopp.setAttribute('data-value', stuffKey)
+    addToppingBtn.removeAttribute("disabled");
+    topppAcList.parentNode.classList.toggle("hidden", true);
+  };
+  
+  Array.from(elemLinks).forEach((el) => 
+    el.addEventListener("click", ev => {
+        choose( ev.target.parentNode.getAttribute("data-stuffing") );
+        ev.preventDefault();
+    })
+  );
   
 }
+
