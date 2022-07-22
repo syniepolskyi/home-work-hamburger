ef0683c30179119e2bb195ef182df41f306f75e6
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-22 21:21:12 +0300

    Update calculate-total.js

--- js/hamburger/calculate-total.js
+++ js/hamburger/calculate-total.js
@@ -1,4 +1,23 @@
 Hamburger.calculateTotal = function(){
-  //Your code here
-  
+  const totalPriceTd = document.getElementById("totalprice");
+  const totalCcalTd = document.getElementById("totalccal");
+  const priceTds = document.querySelectorAll("td[data-price]");
+  const ccalTds = document.querySelectorAll("td[data-ccal]");
+  const amountTds = document.querySelectorAll("td[data-amount]");
+  let totalPrice = priceTds.reduce((acc,enext) => 
+                                   acc 
+                                   + (enext.parentNode.classList.contains("order-template")? 
+                                   0 
+                                      : (parseInt(enext.innerText,10)
+                                      * parseInt(enext.nextSibling.innerText,10))), 
+  0);
+  let totalCcal = ccalTds.reduce((acc,enext) => 
+                                 acc 
+                                 + (enext.parentNode.classList.contains("order-template")? 
+                                 0 
+                                    : (parseInt(enext.innerText,10)
+                                    * parseInt(enext.previousSibling.innerText,10))), 
+  0);
+  totalPriceTd.innerText = totalPrice;
+  totalCcalTd.innerText = totalCcal;
 }
