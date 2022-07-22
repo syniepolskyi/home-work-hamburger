4f85f87484b145f1faec5277cfb1126c328da283
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-22 22:46:13 +0300

    Update autocomplete-size.js

--- js/hamburger/autocomplete-size.js
+++ js/hamburger/autocomplete-size.js
@@ -2,6 +2,8 @@ Hamburger.autocompleteSize = function(){
   const inputSize = document.getElementById("size");
   const sizeAcList = inputSize.nextElementSibling.firstElementChild;
   const htmlTemplate = sizeAcList.firstElementChild.outerHTML;
+  const addToppingBtn = document.getElementById("addtopping");
+  const submitOrderBtn = document.getElementById("submitorder");
   
   const allSizes = Object.keys(Hamburger.sizes).map((k) => {
     let newEl = Hamburger.sizes[k];
@@ -18,6 +20,7 @@ Hamburger.autocompleteSize = function(){
       .map((k) => htmlTemplate )
       .join("\n");
     sizeAcList.innerHTML = '';
+    inputSize.classList.toggle("error", false);
     if(filteredSizes.length){
       sizeAcList.insertAdjacentHTML("beforeend", htmlContent);
       filteredSizes.forEach((hSize,idx) => {
@@ -27,7 +30,10 @@ Hamburger.autocompleteSize = function(){
       });
       return ;
     }
+    inputSize.classList.toggle("error", true);
     sizeAcList.insertAdjacentHTML("beforeend", htmlTemplate);
+    addToppingBtn.setAttribute("disabled","disabled");
+    submitOrderBtn.setAttribute("disabled","disabled");
     sizeAcList.children[0].innerHTML = "<i>нічого не знайдено</i>";
   };
   
