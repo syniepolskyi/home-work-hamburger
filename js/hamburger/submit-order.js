e020052d4f6261ac92d66e29c59171861c141c47
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-23 01:28:58 +0300

    Update submit-order.js

--- js/hamburger/submit-order.js
+++ js/hamburger/submit-order.js
@@ -1,4 +1,10 @@
 Hamburger.submitOrder = function(){
-  //Your code here
-  
+  const h3Elem = document.querySelector(".order-form-container > h3");
+  const orderForm = document.querySelector(".order-form");
+  const submitOrderBtn = document.getElementById("submitorder");
+  submitOrderBtn.addEventListener("click", (ev) => {
+    h3Elem.innerText = "THANK YOU! YOUR ORDER WAS CONFIRMED";
+    orderForm.remove();
+  });
+
 }
