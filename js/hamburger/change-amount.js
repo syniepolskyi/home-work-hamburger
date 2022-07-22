eaa486b7cbb2b00e474cadc31a837bef31bc239d
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-23 00:21:59 +0300

    Update change-amount.js

--- js/hamburger/change-amount.js
+++ js/hamburger/change-amount.js
@@ -2,7 +2,7 @@ Hamburger.changeAmount = function(){
   const amountInput = document.getElementById("amount");
 
   amountInput.addEventListener("change", (ev) => {
-    const tdAmount = dataTr.querySelector(".order-table tbody tr.active-data td.data-amount");
+    const tdAmount = document.querySelector(".order-table tbody tr.active-data td.data-amount");
     if (tdAmount){
       tdAmount.innerText = ev.target.value;
       Hamburger.calculateTotal();
